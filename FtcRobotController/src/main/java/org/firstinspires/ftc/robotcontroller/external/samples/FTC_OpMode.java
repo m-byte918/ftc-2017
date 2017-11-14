8309eaad79b29b9548d1424a73aef2a0299ec57f
Christian <cisr918@gmail.com>
2017-11-14 12:39:12 -0500

    Update FTC_OpMode.java
    Aye the robot works now :D

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/FTC_OpMode.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/FTC_OpMode.java
@@ -30,103 +30,90 @@
 package org.firstinspires.ftc.robotcontroller.external.samples;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name="FTC_OpMode: TeleOp Tank", group="TeleOp")
 @Disabled
-public class FTC_OpMode extends LinearOpMode {
+public class FTC_OpMode extends OpMode {
 
     /* Declare OpMode members. */
     Hardware_FTC_OpMode robot = new Hardware_FTC_OpMode();
 
-    @Override
-    public void runOpMode() {
-        double left;
-        double right;
-        double clawSpeed = 0.5;
-        double clawPosition = robot.CLAW_HOME
-        /* Initialize the hardware variables.
-         * The init() method of the hardware class does all the work here
-         */
+    double left;
+    double right;
+    double clawSpeed = 3;
+    private double clawPosition = robot.CLAW_HOME;
+
+    // Initialize the hardware variables.
+    // The init() method of the hardware class does all the work here
+    public void init(){
         robot.init(hardwareMap);
 
         // Send telemetry message to signify robot waiting;
         telemetry.addData("Say", "Hello Driver");
         telemetry.update();
-
-        // Wait for the game to start (driver presses PLAY)
-        waitForStart();
-
-        // run until the end of the match (driver presses STOP)
-        while (opModeIsActive()) {
-
-            // Run wheels in tank mode (note: The joystick goes negative when pushed forwards, so negate it)
-            left  = -gamepad1.left_stick_y;
-            right = -gamepad1.right_stick_y;
-
-            robot.leftDrive.setPower(left);
-            robot.leftDriveBack.setPower(left);
-            robot.rightDrive.setPower(right);
-            robot.rightDriveBack.setPower(right);
-            
-            // moves arm up and down and in and out 
-            int  up_out  = 10; 
-            int  down_in =-10; 
-            
-            if (gamepad1.a) {
-                robot.extend.setPower(up_out); 
-            } else {
-                robot.extend.setPower(0);
-            }
-            
-            if (gamepad1.b) {
-                robot.extend.setPower(down_in); 
-            } else {
-                robot.extend.setPower(0);
-            }
-        
-            if (gamepad1.dpad.up) {
-                robot.arm.setPower(up_out);
-            } else {
-                robot.arm.setPower(0); 
-            }
-            
-            if ( gamepad1.dpad.down) {
-                robot.arm.setPower(down_in);
-            } else { 
-                robot.arm.setPower(0);
-            }
-            
-            // changes position of claw servo 
-            if (gamepad1.x) {
-                clawPosition += clawSpeed; 
-            } 
-            if (gamepad1.y) {
-                clawPosition -= clawSpeed;
-            } 
-            
-            // sets new position of claw servo 
-            clawPosition = Range.clip(clawPosition, robot.CLAW_MIN_RANGE, robot.CLAW_MAX_RANGE);
-            robot.claw.setPosition(clawPosition);
-
-            // Send telemetry message to signify robot running;
-            telemetry.addLine("Motors and Servo ~")
-            telemetry.addData("left",  "%.2f", left);
-            telemetry.addData("right", "%.2f", right);
-            telemetry.addData("claw",  "%.2f", clawPosition);
-            telemetry.addData("arm", "%.2f", up_out, down_in);
-            telemetry.addData("extend", "%.2f", up_out, down_in); 
-            telemetry.addLine("left joystick ~");
-            telemetry.addData("y1", gamepad1.left_stick_y);
-            telemetry.addLine("right joystick ~");
-            telemetry.addData("y2", gamepad1.right_stick_y);
-            telemetry.update();
-
-            // Pause for 40 mS each cycle = update 25 times a second.
-            sleep(40);
-            // Don't fucking change that xd
-        }
     }
+
+    public void loop() {
+        // Run wheels in tank mode (note: The joystick goes negative when pushed forwards, so negate it)
+        left  = -gamepad1.left_stick_y;
+        right = -gamepad1.right_stick_y;
+
+        robot.leftDrive.setPower(left);
+        robot.leftDriveBack.setPower(left);
+        robot.rightDrive.setPower(right);
+        robot.rightDriveBack.setPower(right);
+
+        // moves arm up and down and in and out
+        int up_out  = 10;
+        int down_in = -10;
+
+        // extends arm & claw
+        if (gamepad1.a && gamepad1.b) {
+            robot.extend.setPower(0);
+        } else {
+            if (gamepad1.a)
+                robot.extend.setPower(up_out);
+            else
+                robot.extend.setPower(0);
+
+            if (gamepad1.b)
+                robot.extend.setPower(down_in);
+            else
+                robot.extend.setPower(0);
+        }
+
+        // moves arm up/down
+        if (gamepad1.dpad_up)
+            robot.arm.setPower(up_out);
+        else
+            robot.arm.setPower(0);
+
+        if (gamepad1.dpad_down)
+            robot.arm.setPower(down_in);
+        else
+            robot.arm.setPower(0);
+
+        // opens and closes claw
+        if (!(gamepad1.x && gamepad1.y)) {
+            if (gamepad1.x && clawPosition <= 0.9)
+                clawPosition += 0.01;
+            if (gamepad1.y && clawPosition >= 0.1)
+                clawPosition -= 0.01;
+        }
+
+        //clawPosition = Range.clip(clawPosition, robot.CLAW_MIN_RANGE, robot.CLAW_MAX_RANGE);
+        robot.claw.setPosition(clawPosition);
+
+        // Send telemetry message to signify robot running;
+        telemetry.addLine("Motors ~");
+        telemetry.addData("left",  "%.2f", left);
+        telemetry.addData("right", "%.2f", right);
+        //telemetry.addData("arm" )
+        telemetry.update();
+    }
+    //give me paper!
+      // no
 }
